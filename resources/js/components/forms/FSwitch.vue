86a2e06794df3dec59f4bff2c199f2f989c12e6a
Albin N <albin@myrmalm.se>
2021-10-28 14:17:30 +0200

    nicer switch labels

--- resources/js/components/forms/FSwitch.vue
+++ resources/js/components/forms/FSwitch.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="flex items-center">
         <span v-if="hasSlot"
-              id="toggleLabel"
-              class="flex flex-col mr-4"
+              :name="name"
+              class="flex flex-col mr-4 "
+              @click="computedValue = ! computedValue"
         >
-            <slot />
+            <FLabel class="mb-0 cursor-pointer select-none"
+                    :name="name"
+            >
+                <slot />
+            </FLabel>
         </span>
         <!-- On: "bg-gold-600", Off: "bg-gray-200" -->
         <button
             :class="{'bg-royal-500': computedValue, 'bg-gray-200': !computedValue}"
-            aria-labelledby="toggleLabel"
+            :aria-labelledby="name"
             aria-pressed="false"
             class="relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-royal-200"
             type="button"
@@ -32,6 +37,10 @@ export default {
         value: {
             type: [Number, Boolean, String],
             default: false
+        },
+        name: {
+            type: String,
+            default: 'switch'
         }
     },
     data () {
