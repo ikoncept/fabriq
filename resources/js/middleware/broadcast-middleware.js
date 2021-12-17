6e7d856622a524d4a1b5d6f9a2cc5a28a78845d9
Albin N <albin@myrmalm.se>
2021-12-17 20:24:41 +0100

    map out broadcast middleware

--- resources/js/middleware/broadcast-middleware.js
+++ resources/js/middleware/broadcast-middleware.js
@@ -6,20 +6,24 @@ export default function BroadcastMiddleware ({ next, to, router, store }) {
     const Echo = router.app.$echo
     const id = to.params.id
     const pusherKey = window.fabriqCms.pusher.key
-    const roomName = to.name
-    const identifier = roomName + '.' + id
 
-    Echo.join(pusherKey + '.presence.' + identifier)
-        .here((users) => {
-            const object = {}
-            object[identifier] = users
-            store.commit('echo/SET_USERS_HERE', object)
+    const broadcastName = to.meta.broadcastName
+    const capitalizedBroadcastName = broadcastName[0].toUpperCase() + broadcastName.slice(1)
+
+    Echo.channel(`${pusherKey}-${broadcastName}.${id}`)
+        .listen(`.${capitalizedBroadcastName}Updated`, (event) => {
+            console.log('broadcast heard, updated on id ', event)
         })
-        .joining((user) => {
-            store.commit('echo/USER_JOINING', { identifier: identifier, user: user })
+
+    Echo.channel(`${pusherKey}-${broadcastName}.`)
+        .listen(`.${capitalizedBroadcastName}Updated`, (event) => {
+            console.log('broadcast heard, updated ', event)
         })
-        .leaving((user) => {
-            store.commit('echo/USER_LEAVING', { identifier: identifier, user: user })
+        .listen(`.${capitalizedBroadcastName}Deleted`, (event) => {
+            console.log('broadcast heard, Deleted ', event)
+        })
+        .listen(`.${capitalizedBroadcastName}Created`, (event) => {
+            console.log('broadcast heard, Created ', event)
         })
 
     return next()
