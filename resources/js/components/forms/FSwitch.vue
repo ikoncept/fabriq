6e151ffdb1b0f2b9e24a89a54fee9467a456f72d
Albin N <albin@myrmalm.se>
2022-01-20 10:11:39 +0100

    add support for showing selected values in switch

--- resources/js/components/forms/FSwitch.vue
+++ resources/js/components/forms/FSwitch.vue
@@ -1,33 +1,65 @@
 <template>
-    <div class="flex items-center">
-        <span v-if="hasSlot"
-              :name="name"
-              class="flex flex-col mr-4 "
-              @click="computedValue = ! computedValue"
+    <div>
+        <FLabel v-if="columnLayout"
+                class="mb-2 cursor-pointer select-none"
+                :name="name"
         >
-            <FLabel class="mb-0 cursor-pointer select-none"
-                    :name="name"
+            <slot />
+        </FLabel>
+        <div
+            class="flex items-center"
+        >
+            <span v-if="hasSlot && ! columnLayout"
+                  :name="name"
+                  class="flex flex-col mr-4 "
+                  @click="computedValue = ! computedValue"
             >
-                <slot />
-            </FLabel>
-        </span>
-        <!-- On: "bg-gold-600", Off: "bg-gray-200" -->
-        <button
-            :class="{'bg-royal-500': computedValue, 'bg-gray-200': !computedValue}"
-            :aria-labelledby="name"
-            aria-pressed="false"
-            class="relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-royal-200"
-            type="button"
-            @click="toggleSwitch"
-        >
-            <span class="sr-only">Use setting</span>
-            <!-- On: "translate-x-5", Off: "translate-x-0" -->
-            <span
-                :class="{'translate-x-5': computedValue, 'translate-x-0':! computedValue}"
-                aria-hidden="true"
-                class="absolute top-0 left-0 inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full shadow ring-0"
-            />
-        </button>
+                <FLabel class="mb-0 cursor-pointer select-none"
+                        :name="name"
+                >
+                    <slot />
+                </FLabel>
+            </span>
+            <!-- On: "bg-gold-600", Off: "bg-gray-200" -->
+            <button
+                :class="{'bg-royal-500': computedValue, 'bg-gray-200': !computedValue}"
+                :aria-labelledby="name"
+                aria-pressed="false"
+                class="relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-royal-200"
+                type="button"
+                @click="toggleSwitch"
+            >
+                <span class="sr-only">Use setting</span>
+                <!-- On: "translate-x-5", Off: "translate-x-0" -->
+                <span
+                    :class="{'translate-x-5': computedValue, 'translate-x-0':! computedValue}"
+                    aria-hidden="true"
+                    class="absolute top-0 left-0 inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full shadow ring-0"
+                />
+            </button>
+            <Transition v-if="columnLayout"
+                        tag="div"
+                        name="fade"
+                        mode="out-in"
+            >
+                <span v-if="computedValue"
+                      key="truthy"
+                      class="ml-2.5 text-sm inline-block"
+                >
+                    <slot name="truthy">
+                        Ja
+                    </slot>
+                </span>
+                <span v-else
+                      key="falsy"
+                      class="ml-2.5 text-sm inline-block"
+                >
+                    <slot name="falsy">
+                        Nej
+                    </slot>
+                </span>
+            </Transition>
+        </div>
     </div>
 </template>
 <script>
@@ -41,6 +73,10 @@ export default {
         name: {
             type: String,
             default: 'switch'
+        },
+        columnLayout: {
+            type: Boolean,
+            default: false
         }
     },
     data () {
