86f94cfd7534bc2fd47c718862a4c9fe3f863408
Albin N <albin@myrmalm.se>
2022-03-14 14:24:03 +0100

    add gs command to create pngs

---
+++ src/Actions/ConvertPdfToImage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ikoncept\Fabriq\Actions;
+
+class ConvertPdfToImage
+{
+    public function __invoke(string $outputPath, string $inputPath, int $firstPage = 1, int $lastPage = 1) : string
+    {
+        $command = ('gs -sDEVICE=png16m \
+            -dBATCH \
+            -dNOPAUSE \
+            -r144 \
+            -dCompatibilityLevel=1.4 \
+            -dOptimize=true \
+            -dColorConversionStrategy=/sRGB \
+            -dProcessColorModel=/DeviceRGB  \
+            -dFirstPage=' . $firstPage . ' \
+            -dLastPage=' . $lastPage . ' \
+            -sOutputFile='. $outputPath  .'-%d.png16m '. $inputPath);
+
+        exec($command);
+
+        return $outputPath;
+    }
+}
