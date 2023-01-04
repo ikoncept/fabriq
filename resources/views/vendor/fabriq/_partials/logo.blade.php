322fd6d2e1492a5f27f77ef6f399f471da20f618
Albin Nilsson <albin@myrmalm.se>
2023-01-04 09:44:52 +0100

    correct path to logo

--- resources/views/vendor/fabriq/_partials/logo.blade.php
+++ resources/views/vendor/fabriq/_partials/logo.blade.php
@@ -1,4 +1,3 @@
 <div class="flex justify-center w-full mb-8 text-4xl">
-    <img src="/images/fabriq-logo-plain.svg" class="h-16" alt="Fabriq CMS Logotyp">
+    <img src="/fabriq/images/fabriq-logo-plain.svg" class="h-16" alt="Fabriq CMS Logotyp">
 </div>
-
