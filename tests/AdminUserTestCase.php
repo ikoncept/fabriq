9cb3a835359ccf2647253a5cba5046574e71256b
Albin N <albin@jnilsson.id>
2024-04-10 10:28:42 +0200

    rename driver

--- tests/AdminUserTestCase.php
+++ tests/AdminUserTestCase.php
@@ -64,8 +64,8 @@ abstract class AdminUserTestCase extends Orchestra
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'pgsql');
-        $app['config']->set('database.connections.pgsql', [
+        $app['config']->set('database.default', 'mysql');
+        $app['config']->set('database.connections.mysql', [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
