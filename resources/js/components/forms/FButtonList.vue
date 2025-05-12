a4c73a2fb5c802fb5739f9d3fde9c4f0288ef3f7
Albin N <albin@jnilsson.id>
2025-05-12 10:53:28 +0200

    draggable fbuttonlists

--- resources/js/components/forms/FButtonList.vue
+++ resources/js/components/forms/FButtonList.vue
@@ -38,52 +38,68 @@
                 </template>
             </UiDashedBox>
         </div>
-        <div
-            v-for="(button, index) in buttons"
-            :key="index"
-            class="flex mb-6 space-x-6"
-        >
-            <FButtonItem
-                v-model="buttons[index]"
-                class="flex-1 col-span-12"
-                :placeholder="placeholder"
-                :pages="pages"
-            />
-            <div
-                v-if="mergedOptions.newTab"
-                class="col-span-2"
-            >
-                <FLabel>
-                    <span class="flex items-center">
 
-                        Öppna i ny flik? <ExternalLinkIcon class="w-4 h-4 ml-2" />
-                    </span>
-                </FLabel>
-                <div class="flex items-center mt-3">
-                    <FSwitch v-model="button.newTab" />
-                    <div
-                        class="ml-2 text-sm"
-                        v-text="button.newTab ? 'Ja' : 'Nej'"
+        <Draggable
+            v-model="buttons"
+            handle=".handle-buttons"
+            tag="ul"
+            v-bind="dragOptions"
+            class="list-group-buttons"
+            :group="{ name: 'buttons', pull: 'clone', put: ['buttons'] }"
+            @end="$emit('input', buttons)"
+        >
+            <li
+                v-for="(button, index) in buttons"
+                :key="`button-${button.id}`"
+                class="flex mb-3 space-x-6 border px-4 py-4 rounded"
+            >
+                <GripVerticalIcon class="cursor-grab block w-4 h-4 text-gray-400 handle-buttons" />
+                <div class="flex items-center flex-1 space-x-6">
+                    <FButtonItem
+                        v-model="buttons[index]"
+                        class="flex-1 col-span-12"
+                        :placeholder="placeholder"
+                        :pages="pages"
                     />
+                    <div
+                        v-if="mergedOptions.newTab"
+                        class="col-span-2"
+                    >
+                        <FLabel>
+                            <span class="flex items-center">
+
+                                Öppna i ny flik? <ExternalLinkIcon class="w-4 h-4 ml-2" />
+                            </span>
+                        </FLabel>
+                        <div class="flex items-center mt-3">
+                            <FSwitch v-model="button.newTab" />
+                            <div
+                                class="ml-2 text-sm"
+                                v-text="button.newTab ? 'Ja' : 'Nej'"
+                            />
+                        </div>
+                    </div>
+                    <div class="flex items-end col-span-1 spliceButton">
+                        <button
+                            type="button"
+                            class="p-4 -m-4 transition-colors duration-200 transform focus:outline-none hover:text-red-600"
+                            @click="spliceButton(index)"
+                        >
+                            <MinusIcon class="w-8 h-8 mb-2" />
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div class="flex items-end col-span-1 spliceButton">
-                <button
-                    type="button"
-                    class="p-4 -m-4 transition-colors duration-200 transform focus:outline-none hover:text-red-600"
-                    @click="spliceButton(index)"
-                >
-                    <MinusIcon class="w-8 h-8 mb-2" />
-                </button>
-            </div>
-        </div>
+            </li>
+        </Draggable>
     </div>
 </template>
 <script>
 import PageTree from '@/models/PageTree.js'
+import Draggable from 'vuedraggable'
 import Menu from '@/models/Menu.js'
 export default {
     name: 'FButtonList',
+    components: { Draggable },
     props: {
         value: {
             type: Array,
@@ -111,10 +127,19 @@ export default {
             defaultOptions: {
                 newTab: false
             },
-            pages: []
+            pages: [],
+            drag: false,
         }
     },
     computed: {
+        dragOptions () {
+            return {
+                animation: 200,
+                group: 'buttons',
+                disabled: false,
+                ghostClass: 'ghost'
+            }
+        },
         noButtons () {
             return this.buttons.length === 0
         },
@@ -133,7 +158,10 @@ export default {
             this.$emit('input', this.buttons)
         }
         this.fetchPages()
-        this.buttons = this.value
+        this.buttons = this.value.map(item => ({
+            ...item,
+            id: item.id ?? ('i' + Math.random().toString(20).substr(2, 6)),
+        }))
     },
     methods: {
         async fetchPages () {
@@ -167,6 +195,7 @@ export default {
         },
         addButton () {
             this.buttons.push({
+                id: 'i' + Math.random().toString(20).substr(2, 6),
                 type: 'link',
                 linkType: 'internal',
                 text: '',
