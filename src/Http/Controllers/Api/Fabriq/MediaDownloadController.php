4557348ce9533d95a27869ef5bc08026bb0907bf
Albin N <albin@jnilsson.id>
2025-12-16 12:30:46 +0100

    allow streamed response

--- src/Http/Controllers/Api/Fabriq/MediaDownloadController.php
+++ src/Http/Controllers/Api/Fabriq/MediaDownloadController.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Storage;
 use Infab\Core\Http\Controllers\Api\ApiController;
 use Infab\Core\Traits\ApiControllerTrait;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class MediaDownloadController extends ApiController
 {
     use ApiControllerTrait;
 
-    public function show(Request $request, string $uuid): BinaryFileResponse
+    public function show(Request $request, string $uuid): BinaryFileResponse|StreamedResponse
     {
         $mediaFile = Fabriq::getModelClass('media')
             ->whereUuid($uuid)->firstOrFail();
